0e27318fe897e8fded512b8d598fa34e55e200a6
PC <kien05071995@gmail.com>
2023-10-02 15:32:13 +0700

    MD3_EX_Login

---
+++ src/main/java/com/example/login_project/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.login_project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
